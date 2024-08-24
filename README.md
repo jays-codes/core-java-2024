6551b1b9cc73970fbf8e28d6cd79bacef8f789a1
jay menorca <zaimenorca@gmail.com>
2024-08-24 16:14:17 -0400

    Added lambda Predicate demo

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Jay's proj repo for Core Java practice. List below ARE NOT NOTES. They are topics covered.
 
 proj: functionalprog (mvn, jdk21, junit5)
+- Added lambda Predicate demo
 - Functional Interface, lambda expression demo
 - project and workspace init; fixed tests
--- functionalprog/src/main/java/jayslabs/corejava/LambdaHworld.java
+++ functionalprog/src/main/java/jayslabs/corejava/LambdaHworld.java
@@ -1,12 +1,30 @@
 package jayslabs.corejava;
 
+import java.util.function.Predicate;
+
 interface Ilambda{
     void lambda();
+}
+
+//Predicate
+interface ILambPredicate<T>{
+    boolean test(T t);
 }
 
 public class LambdaHworld {
     public static void main(String[] args) {
         Ilambda lambda = () -> System.out.println("Yeaaaaoooowww!!! HWorld !!!");
         lambda.lambda();
+
+        ILambPredicate<Integer> isNega = i -> i < 0;
+        System.out.println(isNega.test(-1));
+
+        Predicate<String> isPalindrome = str -> {
+            String rev = new StringBuilder(str).reverse().toString();
+            return str.equals(rev);
+        };
+
+        String str = "madam1";
+        System.out.println(str + " <-- isPalindrome?: " + isPalindrome.test(str));
     }
 }
