fdab7705e85a43f70db74657ec7a5818fa165b39
jay menorca <zaimenorca@gmail.com>
2024-08-31 01:04:13 -0400

    updated bound method reference demo

--- functionalprog/src/main/java/jayslabs/corejava/MethodReferenceDemo.java
+++ functionalprog/src/main/java/jayslabs/corejava/MethodReferenceDemo.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class MethodReferenceDemo {
     public static void main(String[] args) {
@@ -10,18 +11,20 @@ public class MethodReferenceDemo {
         Consumer<String> printer = System.out::println;
         names.forEach(printer);
 
-
-        //sample of Bound method reference
-        System.out.println("Using method reference:");
-        names.forEach(System.out::println);
-        
-
-        names.forEach((name) -> System.out.println(name));
-
         Predicate<String> predicate = String::isEmpty;
         System.out.println(predicate.test(""));
         System.out.println(predicate.test("Hello"));
 
-    
+        //sample of Unbound method reference
+        
+
+
+
+
+        //another sample of bound method reference
+        String prefix = "Hello";
+        Supplier<Integer> lenSupplier = prefix::length;
+        System.out.println(lenSupplier.get());
+
     }
 }
