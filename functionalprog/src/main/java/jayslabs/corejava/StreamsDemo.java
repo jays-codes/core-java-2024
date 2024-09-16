221360d9dac94159fc69c2d4d730eb9ab5ddec2b
jay menorca <zaimenorca@gmail.com>
2024-09-15 22:29:15 -0400

    Demo of Terminal operations: forEach, reduce, collect, min, max, count, anyMatch, allMatch, noneMatch, findFirst, findAny

--- functionalprog/src/main/java/jayslabs/corejava/StreamsDemo.java
+++ functionalprog/src/main/java/jayslabs/corejava/StreamsDemo.java
@@ -1,8 +1,13 @@
 package jayslabs.corejava;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
     public static void main(String[] args) {
@@ -10,11 +15,14 @@ public class StreamsDemo {
 
         //creatingStreams();
         //anymatchAndMapDemo();
-        lazinessDemo();
+        //lazinessDemo();
+        //infiniteStreams();
+        //infiniteStreamsUsingGenerate();
+        streamVsForLoop();
     }
 
     // Creating Streams
-    public static void creatingStreams(){
+    public static void creatingStreamsOld(){
         System.out.println("Names");
         List<String> names = new ArrayList<>();
         names.add("John");
@@ -63,5 +71,73 @@ public class StreamsDemo {
             .forEach(System.out::println);
     }
 
+    public static void creatingStreams(){
+        //Arrays.stream()        
+    }
 
+    //create stream from a file
+    public static void createStreamFromFile(){
+        try (Stream<String> lines = Files.lines(Paths.get("file.txt"))) {
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Infinite Streams using iterate
+    public static void infiniteStreams(){
+        Stream.iterate(2, n -> n + 2)
+            .limit(10)
+            .forEach(System.out::println);
+    }
+
+    //Infinite Streams using generate
+    public static void infiniteStreamsUsingGenerate(){
+        Stream.generate(Math::random)
+            .limit(10)
+            .forEach(System.out::println);
+    }
+
+    //stream vs for loop test of 100,000 ints. measure time taken
+    public static void streamVsForLoop(){
+        List<Integer> ints = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            ints.add(i);
+        }
+
+        //for loop
+        long start = System.currentTimeMillis();
+        int sum = 0;
+        for (int i = 0; i < ints.size(); i++) {
+            sum += ints.get(i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("For loop time: " + (end - start));
+
+
+        //stream
+        start = System.currentTimeMillis();
+        ints.stream()
+            .mapToInt(Integer::intValue)
+            .sum();
+        end = System.currentTimeMillis();
+        System.out.println("Stream time: " + (end - start));
+        
+        //optimized parallel stream
+        start = System.currentTimeMillis();
+        ints.parallelStream()
+            .mapToInt(Integer::intValue)
+            .sum();
+        end = System.currentTimeMillis();
+        System.out.println("Parallel stream time: " + (end - start));
+
+        // Create an IntStream directly
+        IntStream intStream = IntStream.range(0, 1000000);
+
+        start = System.nanoTime();
+        sum = intStream.parallel().sum();
+        end = System.nanoTime();
+        System.out.println("Optimized parallel stream time: " + (end - start) / 1_000_000.0 + " ms");
+        System.out.println("Sum: " + sum);
+    }
 }
