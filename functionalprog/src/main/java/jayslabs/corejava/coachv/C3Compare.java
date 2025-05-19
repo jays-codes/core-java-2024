0254bf906783b7076449ec9d392750b50adb0744
jay menorca <zaimenorca@gmail.com>
2025-05-18 21:20:31 -0400

    C3Compare - used .thenComparing to chain comparators

--- functionalprog/src/main/java/jayslabs/corejava/coachv/C3Compare.java
+++ functionalprog/src/main/java/jayslabs/corejava/coachv/C3Compare.java
@@ -19,21 +19,23 @@ public class C3Compare {
     new Person("John", 20, 6.5),
     new Person("Sara", 21, 5.7),
     new Person("Jane", 21,4.11),
+    new Person("Anya", 21, 5.7),
     new Person("Greg", 35, 5.11));
 
     final static List<Employee> employees = Arrays.asList(
         new Employee("John", 20, 6.5),
         new Employee("Sara", 21, 5.7),
-        new Employee("Jane", 18,4.11),
+        new Employee("Jade", 21, 5.7),
+        new Employee("Jane", 21,4.11),
         new Employee("Greg", 35, 5.11));
 
-    final static Comparator<Employee> age = Comparator.comparing(Employee::age);
+    final static Comparator<Employee> byAge = Comparator.comparing(Employee::age);
     final static Comparator<Employee> descendingAge = Comparator.comparing(Employee::age).reversed(); 
     final static Comparator<Employee> height = Comparator.comparing(Employee::height);
-    final static Comparator<Employee> name = Comparator.comparing(Employee::name);
+    final static Comparator<Employee> byName = Comparator.comparing(Employee::name);
 
     public static void main(String[] args) {
-        //sortingTest();
+        sortingTest();
 
         //System.out.println("youngest employee: ");
         minTest();
@@ -43,12 +45,12 @@ public class C3Compare {
     }
 
     public static void minTest(){
-        employees.stream().min(Comparator.comparing(Employee::age))
+        employees.stream().min(byAge)
         .ifPresent(emp -> System.out.println(emp.name() + " is the youngest employee"));
     }
 
     public static void maxTest(){
-        employees.stream().max(Comparator.comparing(Employee::age))  
+        employees.stream().max(byAge)  
         .ifPresent(emp -> System.out.println(emp.name() + " is the oldest employee"));
     }
 
@@ -68,7 +70,7 @@ public class C3Compare {
         
         //Sorts Employee by age
         List<Employee> sortedEmps = employees.stream()
-        .sorted(descendingAge)
+        .sorted(byAge.thenComparing(byName))
         .toList();
 
         sortedEmps.forEach(System.out::println);
